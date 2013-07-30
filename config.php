0bb6e8b24e46d60c8128800c98d61d43505f5867
Ryan Floyd <ryanfloyd@live.com>
2013-07-29 23:12:53 -0700

    initial version

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'consumerKey'    => 'YYuQlyrjXHVVPyRIwJ1pieatFALz9DRA7xhPxrwI4SgGjegFYO',
+    'consumerSecret' => '',
+    'callbackUrl'    => 'http://login.ryanfloyd.com/callback.php',
+    'siteUrl'        => 'http://www.tumblr.com/oauth/'
+);
