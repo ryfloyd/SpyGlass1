5b0b4536483b9348a09630ea70109791df59a55c
Ryan Floyd <ryanfloyd@live.com>
2013-07-31 12:55:14 -0700

    bug fix

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'consumerKey'    => 'YYuQlyrjXHVVPyRIwJ1pieatFALz9DRA7xhPxrwI4SgGjegFYO',
-    'consumerSecret' => 'POD7X2alh6Jhf3r7wxS3KrlaEtJYCsqWSmGjJw1uLdQzt64l7t',
+    'consumerSecret' => '',
     'callbackUrl'    => 'http://spyglass.ryanfloyd.com/callback.php',
     'siteUrl'        => 'http://www.tumblr.com/oauth/'
 );
