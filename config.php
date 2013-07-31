2b4a38c97ecaa68cb4ff8766eef2facc3e0f2fb9
Ryan Floyd <ryanfloyd@live.com>
2013-07-31 12:52:19 -0700

    merge

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'consumerKey'    => 'YYuQlyrjXHVVPyRIwJ1pieatFALz9DRA7xhPxrwI4SgGjegFYO',
-    'consumerSecret' => '',
-    'callbackUrl'    => 'http://login.ryanfloyd.com/callback.php',
+    'consumerSecret' => 'POD7X2alh6Jhf3r7wxS3KrlaEtJYCsqWSmGjJw1uLdQzt64l7t',
+    'callbackUrl'    => 'http://spyglass.ryanfloyd.com/callback.php',
     'siteUrl'        => 'http://www.tumblr.com/oauth/'
 );
